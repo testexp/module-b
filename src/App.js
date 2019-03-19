0a9a480d5f1531aefb8df9207338087fc8bca558
Sakshi Gupta <sakshgup@adobe.com>
2019-03-19 17:14:13 +0530

    adding module-b

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ModuleB() {
+  return (
+    <div>
+      Hello from Module B
+    </div>
+  );
+}
--- src/TemplateBuilder.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function TemplateBuilder(props) {
-  return (
-    <div className="canvas-item">
-      Hello world
-    </div>
-  );
-}
